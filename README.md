95240bb36936463e05670ac101f57ae97f9ff9c8
z-lamb <lamb.zachary@gmail.com>
2018-09-07 16:59:16 -0600

    Outlined Jet class

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 ### Week 2 Homework for SKill Distillery
 
 This program displays and managers a fleet of Jet objects.
+
+### Overview
+Talk about where we are in the class as of the time of this project
+
+### Technologies/Techniques Used
+
+### Lessons Learned
+
+### Problems/Issues 
--- src/com/skilldistillery/jets/Jet.java
+++ src/com/skilldistillery/jets/Jet.java
@@ -1,5 +1,23 @@
 package com.skilldistillery.jets;
 
 public abstract class Jet {
-
+	private String model;
+	private double speed;
+	private int range;
+	private long price;
+	
+	public Jet(String model, double speed, int range, long price) {
+		
+	}
+	
+	public void fly() {
+		
+	}
+	
+	public double getSpeedInMach() {
+		double output = 0;
+		
+		return output;
+	}
+	
 }
